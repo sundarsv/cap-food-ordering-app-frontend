bcc4a425d86584c73b7fd295b875a19907e548f7
Srinivasan <sundarsv@gmail.com>
2018-12-12 22:46:04 +0530

    implementing UI for menu items

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,15 +3,46 @@ import '../details/Details.css';
 import Header from '../../common/header/Header';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faInr } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faCircle, faRupeeSign } from '@fortawesome/free-solid-svg-icons';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+import Add from '@material-ui/icons/Add';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import CloseIcon from '@material-ui/icons/Close';
 
-library.add(faStar)
 
+library.add(faStar, faCircle, faRupeeSign)
 
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    icon: {
+      margin: theme.spacing.unit,
+    }
+  });
 
 class Details extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            snackBarOpen:false,
+        }
+    }
+
+    addButtonClickHandler = event => {
+        this.setState({snackBarOpen: true});
+    }
+
+    handleClose = event => {
+        this.setState({snackBarOpen: false});
+    }
+
     render() {
+        const { classes } = this.props;
         return (
             <div>
                 <Header />
@@ -26,26 +57,71 @@ class Details extends Component {
                         <div className="rating-cost">
                             <div className="restaurant-rating">
                                 <FontAwesomeIcon icon="star" /> 4.4
-                                <p>Average rating by 658 users</p>
+                                <p className="sub-text">Average rating by <span className="bold">658</span> users</p>
                             </div>
                             <div className="restaurant-avg-cost">
-                                <FontAwesomeIcon icon="star" /> 600
-                                <p>Average cost for two people</p>
+                                <FontAwesomeIcon icon="rupee-sign" /> 600
+                                <p className="sub-text">Average cost for two people</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="menu-items">
+                <p className="cat-heading">Chinese</p>
+                <Divider />
                     <div className="menu-item">
+                    <table width="100%">
+                        <tr>
+                            <td width="10%">
+                                <FontAwesomeIcon className="veg-icon" icon="circle" />
+                            </td>
+                            <td width="50%" className="menu-item-name">
+                                Pizza test
+                            </td>
+                            <td width="30%">
+                                <FontAwesomeIcon icon="rupee-sign"/> 500.00
+                            </td>
+                            <td width="10%">
+                            <IconButton className={classes.button} onClick={this.addButtonClickHandler} >
+                                <Add className={classes.icon} />
+                            </IconButton>
+                            <Snackbar
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                open={this.state.snackBarOpen}
+                                autoHideDuration={6000}
+                                onClose={this.handleClose}
+                                >
+                                <SnackbarContent
+                                    onClose={this.handleClose}
+                                    message="Item added to cart!"
+                                    action={[
+                                        <IconButton
+                                          key="close"
+                                          aria-label="Close"
+                                          color="inherit"
+                                          className={classes.close}
+                                          onClick={this.handleClose}
+                                        >
+                                          <CloseIcon className={classes.icon} />
+                                        </IconButton>,
+                                      ]}
+                                />
+                            </Snackbar>
+                            </td>
+                        </tr>
+                    </table>
 
                     </div>
                 </div>
                 <div className="cart">
-
+                    
                 </div>
             </div>
         )
     }
 }
 
-export default Details;
+export default withStyles(styles)(Details);
