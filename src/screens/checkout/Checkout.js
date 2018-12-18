ec3826a267ac12b7ef8e65726264cf0a3c9dcece
Maulik Trivedi <maulik.trivedi9015@gmail.com>
2018-12-18 19:49:12 +0530

    State variable initialised.

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -3,6 +3,44 @@ import '../checkout/Checkout.css';
 import Header from '../../common/header/Header'
 
 class Checkout extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            id : "",
+            value:"",
+            location:"",
+            tabValue: 0,
+            activeStep: 0,
+            flat:"",
+            city:"",
+            open: false,
+            locality:"",
+            zipcode:"",
+            statename:"",
+            iconClass:"",
+            addressClass:"",
+            selectedIndex:"",
+            flatRequired: "dispNone",
+            cityRequired: "dispNone",
+            stateRequired: "dispNone",
+            zipcodeRequired: "dispNone",
+            localityRequired: "dispNone",
+            incorrectZipcode: "dispNone",
+            orderPlaced: "dispNone",
+            incorrectDetails:"false",
+            address : "" ,
+            categories : [],
+            totalCartItemsValue: "",
+            orderNotificationMessage:"",
+            states:[],
+            selectedAddress:[],
+            cartItems: [],
+            paymentModes: [],
+            addresses: []
+        }
+    }
+    
 
     render() {
         return (
