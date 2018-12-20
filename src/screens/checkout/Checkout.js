69ef9c3f51a883e4d623910b62c9d0e84c6977a5
Srinivasan <sundarsv@gmail.com>
2018-12-20 23:51:12 +0530

    importing react DOM back

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -29,6 +29,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CheckCircle from '@material-ui/icons/CheckCircle';
 import CloseIcon from '@material-ui/icons/Close';
 import Select from '@material-ui/core/Select';
+import ReactDOM from 'react-dom';	
 import MenuItem from '@material-ui/core/MenuItem';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
